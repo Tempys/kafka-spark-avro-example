a35dd69098bf834f77b4760cebf74bfb4f39506f
Mykhailo_Dubovskyi <mikhail.dubik@gmail.com>
2018-07-30 17:16:50 +0300

    add spark joins

--- src/main/scala/test/KafkaSqlJoins.scala
+++ src/main/scala/test/KafkaSqlJoins.scala
@@ -1,7 +1,8 @@
 package test
 
+import io.confluent.kafka.schemaregistry.client.rest.entities.Schema
 import org.apache.spark.SparkConf
-import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.{Dataset, Row, SparkSession}
 import za.co.absa.abris.avro.read.confluent.SchemaManager
 import za.co.absa.abris.avro.schemas.policy.SchemaRetentionPolicies.RETAIN_SELECTED_COLUMN_ONLY
 
@@ -9,23 +10,21 @@ object KafkaSqlJoins {
 
   def main(args: Array[String]): Unit = {
 
-    val topicName = "sea_vessel_position_reports"
+
     val schemaRegistryURL = "http://127.0.0.1:8081"
-    val subjectValueName = topicName + "-value"
+    val topicNamefirst = "telecom_italia_grid"
+    val subjectValueNamefirst = topicNamefirst + "-value"
+    val topicNamesecond = "telecom_italia_data"
+    val subjectValueNamesecond = topicNamesecond + "-value"
 
     import io.confluent.kafka.schemaregistry.client.rest.RestService
     val restService = new RestService(schemaRegistryURL)
-    val valueRestResponseSchema = restService.getLatestVersion(subjectValueName)
+    val valueRestResponseSchema = restService.getLatestVersion(subjectValueNamefirst)
+    val valueRestResponseSchemaSecond = restService.getLatestVersion(subjectValueNamesecond)
 
 
+    println("second"+valueRestResponseSchemaSecond.getId.toString)
     println(valueRestResponseSchema.getId.toString)
-
-    val schemaRegistryConfs = Map(
-      SchemaManager.PARAM_SCHEMA_REGISTRY_URL   -> schemaRegistryURL,
-      SchemaManager.PARAM_SCHEMA_REGISTRY_TOPIC -> topicName,
-      SchemaManager.PARAM_VALUE_SCHEMA_ID -> valueRestResponseSchema.getId.toString
-    )
-
 
     var conf = new SparkConf()
       .setAppName("Write to MemSQL Example")
@@ -40,24 +39,70 @@ object KafkaSqlJoins {
     import za.co.absa.abris.avro.AvroSerDe._
     val spark = SparkSession.builder().master("local").getOrCreate()
 
-    val stream = spark
+    val streamFromFirstTopic = spark
       .read
       .format("kafka")
       .option("kafka.bootstrap.servers", "localhost:9092")
       .option("startingOffsets", "earliest")
-      .option("subscribe", "sea_vessel_position_reports")
+      .option("subscribe",topicNamefirst)
       .load()
-      .fromConfluentAvro("value", None, Some(schemaRegistryConfs))(RETAIN_SELECTED_COLUMN_ONLY) // invoke the library passing over parameters to access the Schema Registry
+      .fromConfluentAvro("value", None, Some(createschemaRegistryConfs(schemaRegistryURL,topicNamefirst,valueRestResponseSchema)))(RETAIN_SELECTED_COLUMN_ONLY) // invoke the library passing over parameters to access the Schema Registry
   /*    .write
       .format("com.memsql.spark.connector")
       .mode("error")
       .save("people.students")*/
 
-        stream
+
+    val streamFromSecondTopic = spark
+      .read
+      .format("kafka")
+      .option("kafka.bootstrap.servers", "localhost:9092")
+      .option("startingOffsets", "earliest")
+      .option("subscribe", topicNamesecond)
+      .load()
+      .fromConfluentAvro("value", None, Some(createschemaRegistryConfs(schemaRegistryURL,topicNamesecond,valueRestResponseSchemaSecond)))(RETAIN_SELECTED_COLUMN_ONLY) // invoke the library passing over parameters to access the Schema Registry
+
+
+ var joinedStream =   streamFromFirstTopic.join(streamFromSecondTopic,streamFromFirstTopic.col("SquareId").startsWith(streamFromSecondTopic.col("SquareId")))
+
+      streamFromFirstTopic
           .write
           .format("console")
           .save()
 
+    streamFromSecondTopic
+      .write
+      .format("console")
+      .save()
+
+
+
+
+    joinedStream.write
+                 .format("console")
+                 .save()
+
+
+
+
+
+
+
   }
+
+
+  def createschemaRegistryConfs(schemaRegistryURL :String,topic: String, schema: Schema):Map[String,String] ={
+
+    val schemaRegistryConfs = Map(
+      SchemaManager.PARAM_SCHEMA_REGISTRY_URL   -> schemaRegistryURL,
+      SchemaManager.PARAM_SCHEMA_REGISTRY_TOPIC -> topic,
+      SchemaManager.PARAM_VALUE_SCHEMA_ID -> schema.getId.toString
+
+    )
+
+    schemaRegistryConfs
+  }
+
+
 
 }
